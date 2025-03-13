8540f8196ac958cfcd670bd6fd674a1138948069
임승민 <imseungmin@imseungmin-ui-MacBookAir.local>
2025-03-13 16:51:44 +0900

    수정: username 변경

--- config.js
+++ config.js
@@ -13,9 +13,9 @@ const siteConfig = {
 const users = [
   {
     id: 0, // default author
-    username: "licat",
-    company: "weniv",
-    position: "CEO",
+    username: "sml",
+    company: "",
+    position: "student",
     img: "img/user/profile-licat.png",
   },
 ];
